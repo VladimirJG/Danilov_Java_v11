f531db2c2db02be48761be59270b37ab6822a8e1
VladimirJG <vladimi85@gmail.com>
2022-11-09 08:04:01 +0300

    corrections for artifacts

--- src/name/Name.java
+++ src/name/Name.java
@@ -2,11 +2,22 @@ package name;
 //Составить алгоритм: если введенное имя совпадает с Вячеслав,
 //то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
 
+import java.util.Scanner;
+
 public class Name {
     public static void main(String[] args) {
-        NameCheck name = new NameCheck();
-        name.enterName();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        enterName(scanner.nextLine());
+    }
+
+    private static void enterName(String name) {
+        if (name.isEmpty()) {
+            System.out.println("Необходимо ввести имя!");
+        } else if (name.equalsIgnoreCase("Вячеслав")) {
+            System.out.println("Привет " + name + "!");
+        } else {
+            System.out.println("Нет такого имени");
+        }
     }
 }
-
-
--- src/number/Number.java
+++ src/number/Number.java
@@ -1,10 +1,32 @@
 package number;
 
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
 //Составить алгоритм: если введенное число больше 7, то вывести “Привет”
 public class Number {
     public static void main(String[] args) {
-        Examination number = new Examination();
-        number.enter();
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        enter(scanner.nextDouble());
+    }
+
+    private static void enter(double num) {
+        try {
+            if (num % 1 != 0) {
+                System.out.println("Введите целое значение");
+            } else if (num > 7) {
+                System.out.println("Привет");
+            } else if (num < 0) {
+                System.out.println("Значение должно быть положительным!");
+            } else {
+                System.out.println("Значение меньше допустимого");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Неверное значение");
+        }
     }
 }
 
